6511730583a4fbc3b299ceb8c7fdf9c5ed253a0f
arj1025 <aj.johnson202@gmail.com>
2014-09-23 19:29:45 -0700

    v4.0
    Complete redesign, added form

---
+++ wrn.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+
+var ref = (''+document.referrer+'');
+            var w_h = window.screen.width + " x " + window.screen.height;
+            document.write('<script src="http://freehostedscripts.net/ocounter.php?site=ID4414178&e1=Worshipping Right Now&e2=Worshipping Right Now&r=' + ref + '&m=0&wh=' + w_h + '"><\/script>'); 
